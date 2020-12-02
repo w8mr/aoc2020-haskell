485ced30d4811291b9fa1a27f92d125883baf4df
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-02 06:55:49 +0100

    Day 2

--- aoc2020-2.hs
+++ aoc2020-2.hs
@@ -1,9 +1,25 @@
 import Advent
+import Data.List.Split (splitOn)
 
-solve = id
+splitInput line = (low, high, letter, password) where
+  parts = splitOn " " line
+  parts2 = splitOn "-" (head parts)
+  low = read (head parts2) :: Int
+  high = read (parts2 !! 1) :: Int
+  letter = head (parts !! 1)
+  password = parts !! 2
 
-solve1 = solve
-solve2 = solve1
+isValid (low, high, letter, password) = low <= count && count <= high where
+  count = length (filter (==letter) password)
+
+isValid2 (low, high, letter, password) = (match1 || match2) && (not (match1 && match2)) where
+  match1 = password !! (low - 1) == letter
+  match2 = password !! (high - 1) == letter
+
+solve f = length . (filter f) . (map splitInput)
+
+solve1 = solve isValid
+solve2 = solve isValid2
 
 main :: IO ()
 main = execute 2 (lines) [
