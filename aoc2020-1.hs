c985a9ff6a5239f016b88e8c2122b8f2816ccb4c
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-04 18:21:16 +0100

    Day 1 using List comprehension

--- aoc2020-1.hs
+++ aoc2020-1.hs
@@ -1,46 +1,13 @@
 import Advent
-import Data.Char(digitToInt)
-
---solve (x:xs) = if ((check x xs) > 0) then check x xs else solve (tail xs)
---
---check n (y:ys) = if n+y == 2020 then n*y else check n ys
---check _ [] = 0
-
-
-
-
-
-
---solve :: Int -> [Int] -> Int
---solve size = solve' . (subsequencesOfSize size) where
---  solve' (xxs:xs) | sum xxs == 2020 = product xxs
---  solve' (xxs:xs) | otherwise       = solve' xs
-
-solve = check . pairsHalf
-
-check ((x, y):xs) = if x + y == 2020 then x * y else check xs
-
-
+import Data.List (tails)
 
 solve1 :: [Int] -> Int
-solve1 = solve' . pairsHalf where
-  solve' ((x, y):xs) | x + y == 2020 = x * y
-  solve' ((x, y):xs) | otherwise     = solve' xs
+solve1 xs = head [ x * y | (x:ys) <- tails xs, y <- ys, x + y == 2020]
 
 solve2 :: [Int] -> Int
-solve2 = solve' . triplesHalf where
-  solve' ((x, y, z):xs) | x + y + z == 2020 = x * y * z
-  solve' ((x, y, z):xs) | otherwise         = solve' xs
+solve2 xs = head [ x * y * z | (x:ys) <- tails xs, (y:zs) <- tails ys, z <- zs, x + y + z == 2020]
 
 main :: IO ()
 main = execute 1 readIntLines [
-  (solve),
+  (solve1),
   (solve2)]
-
-subsequencesOfSize :: Int -> [a] -> [[a]]
-subsequencesOfSize n xs = let l = length xs
-                          in if n>l then [] else subsequencesBySize xs !! (l-n)
- where
-   subsequencesBySize [] = [[[]]]
-   subsequencesBySize (x:xs) = let next = subsequencesBySize xs
-                             in zipWith (++) ([]:next) (map (map (x:)) next ++ [[]])
