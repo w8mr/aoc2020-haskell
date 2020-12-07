ac6f2240ca0b66db6387d0b4e97a89915499e6f4
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-08 00:08:50 +0100

    Day 6

---
+++ aoc2020-6.hs
@@ -0,0 +1,14 @@
+import Advent
+import Data.List (group, sort, nub)
+import Data.List.Split (splitOn)
+
+split :: String -> [[String]]
+split = (map (filter ((>0) . length) . splitOn "\n") . splitOn "\n\n")
+
+solve1 = (sum . map (length . nub. sort . concat))
+solve2 = (sum .map (\(c, a) -> (length . filter (==True) . map (==c) . map length . group .sort) a) . map ((\x -> (length x, concat x))))
+
+main :: IO ()
+main = execute 6 (split) [
+  solve1,
+  solve2 ]
