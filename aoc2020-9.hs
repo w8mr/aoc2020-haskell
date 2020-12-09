88704b7020cf96bf39f4189bc14614448a0ccadf
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-09 15:24:09 +0100

    Cleanup Day 9

--- aoc2020-9.hs
+++ aoc2020-9.hs
@@ -1,22 +1,19 @@
-{-# Language OverloadedStrings #-}
 import Advent
-import Data.List (nub, sort, tails, inits)
-
-sums xs = [x + y | (x:ys) <- tails xs, y <-ys]
+import Data.List (tails, inits)
 
 solve1 = go 25 where
   go n xs | length xs <= n = -1
           | x `elem` s     = go n (tail xs)
           | otherwise      = x where
-    (preamble, xxs) = splitAt n xs
+    (preamble, (x:_)) = splitAt n xs
+    sums xs = [x + y | (x:ys) <- tails xs, y <-ys]
     s = sums preamble
-    x = head xxs
 
 solve2 xs = go (solve1 xs) xs where
-  go n xs = if r == n then s else go n (tail xs) where
+  go n xs | r == n    = s
+          | otherwise = go n (tail xs) where
     initsSums = [ (sum ys, minimum ys + maximum ys) | ys <- inits xs ]
     (r,s) = (last . takeWhile ((<= n) . fst)) initsSums
-
 
 main :: IO ()
 main = execute 9 (parseLines number) [
