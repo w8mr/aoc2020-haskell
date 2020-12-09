ff112ca05e3060b6985b5db5abc0a0ba45bb07ee
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-09 15:45:22 +0100

    Cleanup Day 8

--- aoc2020-8.hs
+++ aoc2020-8.hs
@@ -5,26 +5,25 @@ type Instruction = (String, Int)
 format :: Parser Instruction
 format = (,) <$> many letterChar <* " " <*> number <* eof
 
-
 solve input = go ([], 0, 0) where
   go :: ([Int], Int, Int) -> (Int, Bool)
   go (hit, acc, ip) | elem ip hit = (acc, True)
                     | ip >= (length input) = (acc, False)
-                    | otherwise = go2 (hit, acc, ip) (input !! ip)
-  go2 (hit, acc, ip) ("nop", param) = go ((ip:hit), acc, ip + 1)
-  go2 (hit, acc, ip) ("acc", param) = go ((ip:hit), acc+param, ip + 1)
-  go2 (hit, acc, ip) ("jmp", param) = go ((ip:hit), acc, ip + param)
+                    | otherwise = go ((ip:hit), acc', ip') where
+    process ("nop", param) = (acc        , ip + 1)
+    process ("acc", param) = (acc + param, ip + 1)
+    process ("jmp", param) = (acc        , ip + param)
+    (acc', ip') = process (input !! ip)
 
 solve1 = fst . solve
 
 changeInstruction :: [Instruction] -> Int -> [Instruction]
 changeInstruction input ip = xs ++ [new] ++ ys where
-  (xs,(old:ys)) = splitAt ip input
-  new = c old
-  c ("nop", param) = ("jmp", param)
-  c ("jmp", param) = ("nop", param)
-  c ins = ins
-
+  (xs,((instr,param):ys)) = splitAt ip input
+  new = (change instr, param)
+  change "nop" = "jmp"
+  change "jmp" = "nop"
+  change instr = instr
 
 solve2 input = go 0 where
   go cip = let r = solve $ changeInstruction input cip in
