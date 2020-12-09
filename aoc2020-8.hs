43d9c25502ff8683901b1ff448dd22d2000f5e34
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-09 23:48:27 +0100

    Generalized Day 8... we might need it

--- aoc2020-8.hs
+++ aoc2020-8.hs
@@ -1,35 +1,66 @@
 {-# Language OverloadedStrings #-}
 import Advent
 
-type Instruction = (String, Int)
-format :: Parser Instruction
-format = (,) <$> many letterChar <* " " <*> number <* eof
+data Param = Direct Int | Relative Int deriving (Show, Eq)
+data Instruction = Nop Param | Acc Param | Jmp Param deriving (Show, Eq)
+data RunState = Normal | Halt | Loop deriving (Show, Eq)
+data Registers = Registers { acc :: Int} deriving (Show, Eq)
+data State = State { usedIp :: [Int], regs :: Registers, ip :: Int, runState :: RunState} deriving (Show, Eq)
 
-solve input = go ([], 0, 0) where
-  go :: ([Int], Int, Int) -> (Int, Bool)
-  go (hit, acc, ip) | elem ip hit = (acc, True)
-                    | ip >= (length input) = (acc, False)
-                    | otherwise = go ((ip:hit), acc', ip') where
-    process ("nop", param) = (acc        , ip + 1)
-    process ("acc", param) = (acc + param, ip + 1)
-    process ("jmp", param) = (acc        , ip + param)
-    (acc', ip') = process (input !! ip)
+format, fNop, fAcc, fJmp  :: Parser Instruction
+fNop = Nop . Direct <$ "nop " <*> number <* eof
+fAcc = Acc . Direct <$ "acc " <*> number <* eof
+fJmp = Jmp . Relative <$ "jmp " <*> number <* eof
+format = fNop <|> fAcc <|> fJmp
 
-solve1 = fst . solve
+process :: State -> Instruction -> State
+process state (Nop (Direct param))   = changeIpBy 1 state
+process state (Acc (Direct param))   = changeIpBy 1 (changeAccBy param state)
+process state (Jmp (Relative param)) = changeIpBy param state
+
+initState :: State
+initState = State [] (Registers 0) 0 Normal
+
+updateRunState :: RunState -> State -> State
+updateRunState new (State usedIp (Registers acc) ip runState) = State usedIp (Registers acc) ip new
+loop = updateRunState Loop
+halt = updateRunState Halt
+
+updateUsedIp :: Int -> State -> State
+updateUsedIp added (State usedIp (Registers acc) ip runState) = State (added:usedIp) (Registers acc) ip runState
+
+changeAccBy :: Int -> State -> State
+changeAccBy new (State usedIp (Registers acc) ip runState) = State usedIp (Registers (acc + new)) ip runState
+
+changeIpBy :: Int -> State -> State
+changeIpBy diff (State usedIp regs ip runState) = State usedIp regs (ip + diff) runState
+
+solve :: [Instruction] -> State
+solve instructions = go initState where
+  go :: State -> State
+  go state@(State usedIp (Registers acc) ip' runState)
+    | ip' `elem` usedIp = loop state
+    | ip' >= length instructions = halt state
+    | otherwise = go (updateUsedIp ip' (process state (instructions !! (ip state))))
+
+solve1 = acc . regs . solve
 
 changeInstruction :: [Instruction] -> Int -> [Instruction]
 changeInstruction input ip = xs ++ [new] ++ ys where
-  (xs,((instr,param):ys)) = splitAt ip input
-  new = (change instr, param)
-  change "nop" = "jmp"
-  change "jmp" = "nop"
+  (xs,(instr:ys)) = splitAt ip input
+  new = change instr
+  change (Nop (Direct param)) = Jmp (Relative param)
+  change (Jmp (Relative param)) = Nop (Direct param)
   change instr = instr
 
 solve2 input = go 0 where
-  go cip = let r = solve $ changeInstruction input cip in
-             if snd r then go (cip + 1) else fst r
+  go :: Int -> Int
+  go cip = go' (runState state) where
+     state = solve $ changeInstruction input cip
+     go' Halt = (acc . regs) state
+     go' Loop = go (cip + 1)
 
 main :: IO ()
 main = execute 8 (parseLines format) [
-  solve1 ,
-  solve2  ]
+  solve1,
+  solve2 ]
