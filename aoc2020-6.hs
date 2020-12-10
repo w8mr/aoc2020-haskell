8b32f0b60497df24c8a4910ca69fb80732e649b4
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-10 23:16:32 +0100

    Cleanup/Comments Day 6

--- aoc2020-6.hs
+++ aoc2020-6.hs
@@ -2,11 +2,17 @@ import Advent
 import Data.List (group, sort, nub)
 import Data.List.Split (splitOn)
 
-split :: String -> [[String]]
+split :: String -> [[[Char]]]
 split = (map (filter ((>0) . length) . splitOn "\n") . splitOn "\n\n")
 
-solve1 = (sum . map (length . nub. sort . concat))
-solve2 = (sum .map (\(c, a) -> (count id . map (==c) . map length . group .sort) a) . map ((\x -> (length x, concat x))))
+-- Map over all groups, within the group take all letters,
+-- deduplicate(nub), get the length. Sum over all lengths
+solve1 = sum . map (length . nub . concat)
+
+-- Map over all groups, make tuple with number of people and all letters)
+-- Map over tuples. sort and group the letters.
+-- Get Length of groups, count lengths which are same as number of people. Sum count
+solve2 = sum . map (\(c, a) -> (count (==c) . map length . group .sort) a) . map ((\x -> (length x, concat x)))
 
 main :: IO ()
 main = execute 6 (split) [
