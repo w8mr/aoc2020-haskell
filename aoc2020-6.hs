8eea03a43c6b126895e91e2106bfe144e2c449a8
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-10 23:46:12 +0100

    Refactored Day 6

--- aoc2020-6.hs
+++ aoc2020-6.hs
@@ -1,18 +1,15 @@
 import Advent
-import Data.List (group, sort, nub)
+import Data.List (group, sort, nub, union, intersect)
 import Data.List.Split (splitOn)
 
 split :: String -> [[[Char]]]
 split = (map (filter ((>0) . length) . splitOn "\n") . splitOn "\n\n")
 
--- Map over all groups, within the group take all letters,
--- deduplicate(nub), get the length. Sum over all lengths
-solve1 = sum . map (length . nub . concat)
-
--- Map over all groups, make tuple with number of people and all letters)
--- Map over tuples. sort and group the letters.
--- Get Length of groups, count lengths which are same as number of people. Sum count
-solve2 = sum . map (\(c, a) -> (count (==c) . map length . group .sort) a) . map ((\x -> (length x, concat x)))
+-- Map over all groups. Fold over answers of persons with union/intersect
+-- Count combined answers. Sum over groups
+solve f = sum . map (length . foldl1 f)
+solve1 = solve union
+solve2 = solve intersect
 
 main :: IO ()
 main = execute 6 (split) [
