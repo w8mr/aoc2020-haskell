d443a2c61ea738d3b48b414da13a6ee5c5b3406e
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-10 15:20:46 +0100

    Small cleanup Day 10

--- aoc2020-10.hs
+++ aoc2020-10.hs
@@ -9,19 +9,17 @@ solve1 xs = go (sort (0:xs)) 0 0 where
                   | (x+2) `elem` xs = go xs c1 c3
                   | (x+3) `elem` xs = go xs c1 (c3 + 1)
 
-
 solve2 :: [Integer] -> Integer
 solve2 xs = (product . (map (go . sort)) . brk . sort) (0:xs) where
   go (x:[]) = 1
   go (x:xs) = p c where
     p n = sum [go (drop (i-1) xs) | i <- [1..n]]
-    c = sum [fromEnum $ (x+i) `elem` (take 3 xs) | i <- [1..3]]
+    c = sum [fromEnum $ (x+i) `elem` xs | i <- [1..3]]
   brk = go [] [] where
     go a1 a2 (x:[]) = ((x:a2):a1)
     go a1 a2 (x:y:xs) | x+3 == y  = go ((x:a2):a1) [x] (y:xs)
                       | otherwise = go a1 (x:a2) (y:xs)
-
 main :: IO ()
 main = execute 10 (parseLines number) [
   solve1 ,
-  solve2  ]
+  solve2 ]
