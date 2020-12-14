933f77fe0dd412cb27e2c81dd5c128ad67363f04
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-14 09:31:59 +0100

    Cleanup Day 9

--- aoc2020-9.hs
+++ aoc2020-9.hs
@@ -5,7 +5,7 @@ solve1 = go 25 where
   go n xs | x `elem` sums = go n (tail xs)
           | otherwise     = x where
     (preamble, (x:_)) = splitAt n xs
-    sums = [x + y | (x:ys) <- tails preamble, y <-ys]
+    sums = [x + y | (x:ys) <- tails preamble, y <- ys]
 
 solve2 xs = go (solve1 xs) xs where
   go n xs | r == n    = s
