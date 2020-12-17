ea5535975ea9f2f5caf54ef42a78ae897f1f7422
Elmar Wachtmeester <elmar.wachtmeester@ing.com>
2020-12-17 16:45:10 +0100

    Cleanup Day 17

--- aoc2020-17.hs
+++ aoc2020-17.hs
@@ -9,7 +9,6 @@ type Grid4d = [(Int,[(Int,[(Int,[Int])])])]
 type Coord = (Int,Int,Int,Int)
 type Cell = (Coord, Bool)
 
-
 minimum' [] = 0
 minimum' x = minimum x
 
@@ -35,16 +34,16 @@ rangeW grid = [(traverse minimum')..(traverse maximum')] where
 extend :: [Int] -> [Int]
 extend range = [(pred (minimum' range))..(succ (maximum' range))]
 
-run :: [Coord] -> Grid4d -> Grid4d
-run dir grid = grid' where
+run :: ([Int] -> [Int]) -> Grid4d -> Grid4d
+run f grid = grid' where
   rules :: Coord -> Bool -> Bool
-  rules point active = apply active (countAdjecent dir grid point) where
+  rules point active = apply active (countAdjecent grid point) where
     apply False c | c == 3 = True
                   | otherwise = False
     apply True  c | c >= 2 && c <= 3 = True
                   | otherwise = False
   grid' = [runCube (w, fromMaybe [] (lookup w grid)) layersRange rowsRange cellsRange | w <- cubesRange] where
-    cubesRange = extend (rangeW grid)
+    cubesRange = f (rangeW grid)
     layersRange = extend (rangeZ grid)
     rowsRange = extend (rangeY grid)
     cellsRange = extend (rangeX grid)
@@ -55,8 +54,8 @@ run dir grid = grid' where
     runRow ::  Int -> Int -> (Int, [Int]) -> [Int] -> (Int, [Int])
     runRow w z (y,activeCells) cellsRange = (y, [x | x <- cellsRange, rules (x,y,z,w) (elem x activeCells)] )
 
-displayGrid3 :: Grid4d -> IO ()
-displayGrid3 grid = do
+displayGrid :: Grid4d -> IO ()
+displayGrid grid = do
   let cubesRange = rangeW grid
   let layersRange = rangeZ grid
   let rowsRange = rangeY grid
@@ -73,23 +72,22 @@ index :: Grid2d -> Grid4d
 index grid = [(0,[(0,cells)])] where
   cells = [(y-1,[x-1 | x <- [0..(pred (length row))], (row !! x) == '#']) | y <- [0..(pred (length grid))], let row = grid !! y]
 
-solve :: [Coord] -> Grid2d -> Int
-solve dir grid2d = go 6 (index grid2d) where
-  go 0 grid = countActive3 grid where
-  go n grid = go (n-1) (run dir grid)
+solve :: ([Int] -> [Int]) -> Grid2d -> Int
+solve f grid2d = go 6 (index grid2d) where
+  go 0 grid = countActive grid where
+  go n grid = go (n-1) (run f grid)
 
-solve1 = solve dir3
-solve2 = solve dir4
+solve1 = solve id
+solve2 = solve extend
 
 
-countActive3 :: Grid4d -> Int
-countActive3 grid = sum (map (\(w,cube) -> sum (map (\(z,layer) -> sum (map (\(y, row) -> length row) layer)) cube)) grid)
+countActive :: Grid4d -> Int
+countActive grid = sum (map (\(w,cube) -> sum (map (\(z,layer) -> sum (map (\(y, row) -> length row) layer)) cube)) grid)
 
-dir3 = [ (x,y,z,w) | x <- [-1..1], y <- [-1..1], z <- [-1..1], w <- [0], x/=0 || y/=0 || z/=0 || w/=0]
-dir4 = [ (x,y,z,w) | x <- [-1..1], y <- [-1..1], z <- [-1..1], w <- [-1..1], x/=0 || y/=0 || z/=0 || w/=0]
+dir = [ (x,y,z,w) | x <- [-1..1], y <- [-1..1], z <- [-1..1], w <- [-1..1], x/=0 || y/=0 || z/=0 || w/=0]
 
-countAdjecent :: [Coord] -> Grid4d -> Coord -> Int
-countAdjecent dir grid point = count (isActive . shift point) dir where
+countAdjecent :: Grid4d -> Coord -> Int
+countAdjecent grid point = count (isActive . shift point) dir where
   shift (x,y,z,w) (m1,m2,m3,m4) = (x + m1, y + m2, z + m3,w + m4)
   isActive (x,y,z,w) = x `elem` (fromMaybe [] (lookup y (fromMaybe [] (lookup z (fromMaybe [] (lookup w grid ))))))
 
